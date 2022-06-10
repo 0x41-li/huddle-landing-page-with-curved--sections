3f69ffde7a0afaf4e73248589c0960adb8b22af3
Ali Sbane <ali.sbane.dev@gmail.com>
2022-06-11 00:40:24 +0100

    Change some values and add some markup to make the website look as close as possible to the design

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -13,14 +13,17 @@ import communitiesIcon from "../assets/images/icon-communities.svg";
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>Build The Community Your Fans Will Love</h1>
-      <p className="hero__p">
-        Huddle re-imagines the way we build communities. You have a voice, but
-        so does your audience. Create connections with your users as you engage
-        in genuine discussion.
-      </p>
-      <button className="hero__btn">Get Started For Free</button>
-      <img className="hero__img" src={heroImage} alt="" />
+      <div className="hero__intro">
+        <h1>Build The Community Your Fans Will Love</h1>
+        <p className="hero__p">
+          Huddle re-imagines the way we build communities. You have a voice, but
+          so does your audience. Create connections with your users as you
+          engage in genuine discussion.
+        </p>
+        <button className="hero__btn">Get Started For Free</button>
+        <img className="hero__img" src={heroImage} alt="" />
+      </div>
+
       <div className="hero__numbers">
         <div className="hero__numbers__box">
           <img
@@ -32,7 +35,11 @@ export default function Hero() {
           <p className="hero__numbers-box__para">Communities Formed</p>
         </div>
         <div className="hero__numbers__box">
-          <img className="hero__numbers-box__icon hero__message-icon" src={messageIcon} alt="" />
+          <img
+            className="hero__numbers-box__icon hero__message-icon"
+            src={messageIcon}
+            alt=""
+          />
           <span className="hero__numbers-box__number">2.7m+</span>
           <p className="hero__numbers-box__para">Messages Sent</p>
         </div>
